68395fb8c957a424bd4ee0a47b603521233be637
BeastWolf691 <sriver@hotmail.fr>
2024-06-12 16:57:12 +0200

    middle et desk.css

--- view/middle.php
+++ view/middle.php
@@ -57,9 +57,9 @@
                 <button type="button" class="btn btn-primary">Appliquer</button>
             </div>
         </div>
-        <!-- <div class="resultat">
+        <div class="resultat">
             Résultat + infos
-        </div> -->
+        </div>
 
         <div class="contenu">
             <div class="card"></div>
@@ -139,21 +139,4 @@
             Pierre Croce 1,1 M<br>
             Clara Marz 1,1 M<br> -->
         </div>
-        <nav aria-label="Page navigation example">
-            <ul class="pagination">
-                <li class="page-item">
-                    <a class="page-link" href="#" aria-label="Previous">
-                        <span aria-hidden="true">&laquo;</span>
-                    </a>
-                </li>
-                <li class="page-item"><a class="page-link" href="#">1</a></li>
-                <li class="page-item"><a class="page-link" href="#">2</a></li>
-                <li class="page-item"><a class="page-link" href="#">3</a></li>
-                <li class="page-item">
-                    <a class="page-link" href="#" aria-label="Next">
-                        <span aria-hidden="true">&raquo;</span>
-                    </a>
-                </li>
-            </ul>
-        </nav>
     </div>

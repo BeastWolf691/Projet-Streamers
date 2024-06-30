7dd1e52c2bcd508fcc8c89779e799d003c9b69c1
Steven Riviere <sriver@hotmail.fr>
2024-06-30 21:34:21 +0200

    màj

--- view/footer.php
+++ view/footer.php
@@ -1,10 +1,10 @@
-<div class="footer">
+<footer>
     <a href="">Mentions légales</a>
     <a href="">Politique de confidentialité</a>&nbsp;
     <strong>Design &amp; dev :</strong><a href="" class="team">StreamFish.Dev</a>
 
 
-</div>
+</footer>
 
 
 

5490c1a10eb57c728e9ae70e9b56852aae99355c
a.nauleau@hotmail.fr <a.nauleau@hotmail.fr>
2024-07-22 21:07:55 +0200

    liaison formulaireBDD

--- view/logIn.php
+++ view/logIn.php
@@ -1,16 +1,61 @@
 <?php
 include 'header.php';
+
+if (!empty($_POST)) {
+
+
+
+    $errors = array();
+
+    $nickname = htmlspecialchars($_POST['nickname']);
+    $password = htmlspecialchars($_POST['password']);
+    $sql = "SELECT nickname, password FROM users WHERE nickname=:val"; // val  est un motif
+
+    $pdoStatement = $pdo->prepare($sql);
+    if ($pdoStatement) {
+        $pdoStatement->execute(['val' => $nickname]); // transforme le motif
+        $result = $pdoStatement->fetch();
+        // on veut comparer le mot de passe saisie depuis le formulaire avec
+        // le mot de passe haché récupéré depuis la base de do nnées
+
+        if ($result) {
+            if (password_verify($password, $result->password)) {
+                echo "<div class=\"alert alert-success\" role=\"alert\">Connexion réussie</div>";
+
+                sleep(1);
+                header('location:index.php');
+            } else {
+                $errors['password'] = "Mot de passe invalide";
+            }
+        } else {
+            $errors['name'] = "Identifiant invalide";
+        }
+    }
+}
+
 ?>
 <div class="form-container">
-    <form>
-        <label for="username">Pseudo :</label><br>
-        <input type="text" id="username" name="username" required placeholder="Pseudo"><br>
+    <?php if (!empty($errors)) : ?>
+        <div class="erreur">
+            <div class="alert alert-secondary" role="alert">
+                Vous n'avez pas rempli le formulaire correctement
+            </div>
+
+            <?php foreach ($errors as $error) : ?>
+                <div><?php echo "<div class=\"alert alert-danger\" role=\"alert\">$error</div>"; ?></div>
+            <?php endforeach; ?>
+        </div>
+    <?php endif; ?>
+    <form method="post" action="">
+
+        <label for="nickname">Pseudo :</label><br>
+        <input type="text" id="nickname" name="nickname" required placeholder="Pseudo">
 
         <label for="password">Mot de passe :</label><br>
-        <input type="text" id="password" name="password" required placeholder="Mot de passe"><br>
+        <input type="password" id="password" name="password" required placeholder="Mot de passe">
 
-        <label for="feedback">Commentaires :</label><br>
-        <input type="text" id="feedback" name="feedback" required><br>
+        <!--   <label for="feedback">Commentaires :</label><br>
+        <input type="text" id="feedback" name="feedback" required><br> -->
 
         <input type="submit" value="Soumettre"><br>
     </form>
--- view/register.php
+++ view/register.php
@@ -1,33 +1,52 @@
-<?php 
+<?php
 include 'header.php';
+if (!empty($_POST)) {
+    $password = $_POST['password'];
+    $passwordHache = password_hash($password, PASSWORD_DEFAULT);
+
+    if (empty($errors)) {
+        $req = $pdo->prepare("INSERT INTO users SET name=?,lastName=?,nickname=?,password=?,mail=?,birthday=?,dateToSign=?,deck=?");
+        $req->execute([$_POST['name'], $_POST['lastName'], $_POST['nickname'], $passwordHache, $_POST['mail'], $_POST['birthday'],  $_POST['dateToSign'], $_POST['deck']]);
+        sleep(1);
+        header('location:logIn.php');
+    }
+}
 ?>
+
 <form action="" method="post" class="form-container">
 
     <label for="name">Nom</label>
-    <input type="text" name="name" id="name">
+    <input type="text" name="name" id="name" required>
 
     <label for="lastName">Prénom</label>
-    <input type="text" name="lastName" id="lastName">
+    <input type="text" name="lastName" id="lastName" required>
 
     <label for="mail">Mail</label>
     <input type="text" name="mail" id="mail">
 
-    <label for="username">Pseudo</label>
-    <input type="text" name="username" id="username">
+    <label for="nickname">Pseudo</label>
+    <input type="text" name="nickname" id="nickname" required>
+
+    <label for="birthday">Anniversaire</label>
+    <input type="date" name="birthday" id="birthday">
+
+    <label for="dateToSign">Date d'enregistrement'</label>
+    <input type="date" name="dateToSign" id="dateToSign">
 
     <label for="password">Mot de passe</label>
-    <input type="text" name="password" id="password">
+    <input type="password" name="password" id="password" required>
 
-    <label for="confirmPassword">Confirmer mdp</label>
-    <input type="text" name="confirmPassword" id="confirmPassword">
+    <label for="deck">deck</label>
+    <input type="text" name="deck" id="deck">
 
-    <input type="checkbox" name="cgu" id="cgu" require>J'ai lu les CGU
+    <!--  <label for="confirmPassword">Confirmer mdp</label>
+    <input type="text" name="confirmPassword" id="confirmPassword"> -->
 
-    <input type="submit" value="Connexion">
+    <!-- <input type="checkbox" name="cgu" id="cgu" require>J'ai lu les CGU -->
 
-    <!-- mettre un captcha en php -->
+    <input type="submit">
+
 </form>
-
-<?php 
+<?php
 include 'footer.php';
 ?>
